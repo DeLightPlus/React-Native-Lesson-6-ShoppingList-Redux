6137c5bb96390bd62f33b05f530045b03b6cdebc
DeLightPlus <matlakalakabelo1@gmail.com>
2025-02-18 07:42:06 +0200

    Delete unwanted files from pg-node-server

--- redux/persistConfig.js
+++
@@ -1,9 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { persistReducer } from 'redux-persist';
-
-const persistConfig = {
-    key: 'root',
-    storage: AsyncStorage,
-};
-
-export default persistConfig;
